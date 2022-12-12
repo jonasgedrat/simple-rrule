9f1cff9d057ff6d7c07794b7c632af7444a72230
Jonas Gedrat <jonasgedrat@outlook.com>
2022-12-12 15:08:34 -0300

    teste com  tsdx

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * as Types from './types'
+export * as expandRRule from './expandRrule'
+export * as getRRuleString from './getRrule'
--- test/expandRruleJSDate.test.ts
+++ test/expandRruleJSDate.test.ts
@@ -1,16 +1,19 @@
-import { describe, expect, it, jest } from '@jest/globals'
-import { addDays, addHours, addMinutes, addYears, startOfDay } from 'date-fns'
-import { expandRRule } from '../expandRrule'
-import { parseRecurrenceFromString } from '../parseRrule'
-import { toRRuleDateString } from '../rRuleDateStringFormat'
-import { Frequency, Weekday } from '../types'
+import { expandRRule } from '../src/expandRrule'
+import { parseRecurrenceFromString } from '../src/parseRrule'
+import { toRRuleDateString } from '../src/rRuleDateStringFormat'
+import { Frequency, Weekday } from '../src/types'
 
-const today = addDays(startOfDay(new Date()), -1)
-const startPeriod = addYears(today, -2)
-const endPeriod = addYears(today, 10)
+const year = 2020
+const month = 1
+const day = 1
+const hour = 0
+const minute = 0
+
+const today = new Date(year, month, day, hour, minute)
+const startPeriod = new Date(year - 2, month, day, hour, minute)
+const endPeriod = new Date(year + 10, month, day, hour, minute)
 
 const dtStart = `DTSTART:${toRRuleDateString(today)}`
-// const dtUntil = `UNTIL=${toRRuleDateString(addMonths(today, 2))}`
 
 test('expandRRule variables tests', () => {
     expect(today).toBeInstanceOf(Date)
@@ -18,7 +21,7 @@ test('expandRRule variables tests', () => {
     expect(startPeriod).toBeInstanceOf(Date)
 })
 
-const intervals = [1, 2, 3]
+const intervals = [1, 3]
 const frequencies = [
     Frequency.MINUTELY,
     Frequency.HOURLY,
@@ -32,7 +35,7 @@ const frequencies = [
 intervals.map((interval) => {
     // eslint-disable-next-line array-callback-return
     frequencies.map((frequency) => {
-        test(`expandRRule date-fns frequency: ${frequency} interval: ${interval} count: 3`, () => {
+        test(`expandRRule JS Date frequency: ${frequency} interval: ${interval} count: 3`, () => {
             const rRuleString = `${dtStart}\nRRULE:FREQ=${frequency};INTERVAL=${interval};COUNT=3;WKST=SU`
 
             const rRule = parseRecurrenceFromString(rRuleString, Weekday.Sunday)
@@ -44,6 +47,7 @@ intervals.map((interval) => {
                 expect(rRule.count).toEqual(3)
 
                 const ex = expandRRule(rRule, startPeriod, endPeriod)
+
                 expect(ex).not.toBeUndefined()
                 expect(ex.events.length).toEqual(3)
                 expect(ex.events[0].date).toBeInstanceOf(Date)
@@ -51,32 +55,51 @@ intervals.map((interval) => {
                 // eslint-disable-next-line array-callback-return
                 ex.events.map((e, i) => {
                     expect(e.index).toEqual(i + 1)
-
                     switch (frequency) {
                         case Frequency.MINUTELY:
                             expect(e.date).toEqual(
-                                addMinutes(today, i * interval)
+                                new Date(
+                                    year,
+                                    month,
+                                    day,
+                                    hour,
+                                    minute + i * interval
+                                )
                             )
                             break
                         case Frequency.HOURLY:
                             expect(e.date).toEqual(
-                                addHours(today, i * interval)
+                                new Date(
+                                    year,
+                                    month,
+                                    day,
+                                    hour + i * interval,
+                                    minute
+                                )
                             )
                             break
                         case Frequency.DAILY:
-                            expect(e.date).toEqual(addDays(today, i * interval))
+                            expect(e.date).toEqual(
+                                new Date(
+                                    year,
+                                    month,
+                                    day + i * interval,
+                                    hour,
+                                    minute
+                                )
+                            )
                             break
                         case Frequency.WEEKLY:
                             expect(e.date).toEqual(
-                                addDays(today, i * interval * 7)
+                                new Date(
+                                    year,
+                                    month,
+                                    day + i * interval * 7,
+                                    hour,
+                                    minute
+                                )
                             )
                             break
-                        // case Frequency.MONTHLY:
-                        //     expect(e.date).toEqual(addMonths(today, i * interval))
-                        //     break
-                        // case Frequency.YEARLY:
-                        //     expect(e.date).toEqual(addYears(today, i * interval))
-                        //     break
 
                         default:
                     }
