7b4c6a7b7e73019e9e29d8018670f37e57b8a526
Jonas Gedrat <jonasgedrat@outlook.com>
2023-02-01 08:40:14 -0300

    fix: correct Test getWeekDayName timezone

---
+++ COMMIT_STYLES.md
@@ -0,0 +1,4 @@
+to commit:
+
+git add.
+npm run commit
--- test/dates/getWeekDayName.test.ts
+++ test/dates/getWeekDayName.test.ts
@@ -1,11 +1,11 @@
 import { getWeekDayName } from '../../src/dates'
 
 test('getWeekDayName', () => {
-    expect(getWeekDayName(new Date('2020-01-06'))).toEqual('SU')
-    expect(getWeekDayName(new Date('2020-01-07'))).toEqual('MO')
-    expect(getWeekDayName(new Date('2020-01-01'))).toEqual('TU')
-    expect(getWeekDayName(new Date('2020-01-02'))).toEqual('WE')
-    expect(getWeekDayName(new Date('2020-01-03'))).toEqual('TH')
-    expect(getWeekDayName(new Date('2020-01-04'))).toEqual('FR')
-    expect(getWeekDayName(new Date('2020-01-05'))).toEqual('SA')
+    expect(getWeekDayName(new Date('2020-01-05T15:20:30.000Z'))).toEqual('SU')
+    expect(getWeekDayName(new Date('2020-01-06T15:20:30.000Z'))).toEqual('MO')
+    expect(getWeekDayName(new Date('2020-01-07T15:20:30.000Z'))).toEqual('TU')
+    expect(getWeekDayName(new Date('2020-01-08T15:20:30.000Z'))).toEqual('WE')
+    expect(getWeekDayName(new Date('2020-01-09T15:20:30.000Z'))).toEqual('TH')
+    expect(getWeekDayName(new Date('2020-01-10T15:20:30.000Z'))).toEqual('FR')
+    expect(getWeekDayName(new Date('2020-01-11T15:20:30.000Z'))).toEqual('SA')
 })
